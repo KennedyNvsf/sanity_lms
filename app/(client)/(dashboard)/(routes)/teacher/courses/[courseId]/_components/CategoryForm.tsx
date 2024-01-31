fc6d149ba82e5bc295a0ef83ce21655ab3b327d1
Webstack-Corporation <webstackcorp@gmail.com>
2024-01-31 13:15:09 +0000

    parsing error fix for deployment

--- app/(client)/(dashboard)/(routes)/teacher/courses/[courseId]/_components/CategoryForm.tsx
+++ app/(client)/(dashboard)/(routes)/teacher/courses/[courseId]/_components/CategoryForm.tsx
@@ -104,7 +104,7 @@ export const CategoryForm = ({
                 <FormItem>
                   <FormControl>
                     <Combobox
-                      options={...options}
+                      options={options}
                       {...field}
                     />
                   </FormControl>
